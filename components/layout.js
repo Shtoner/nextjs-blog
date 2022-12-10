ab26a6bf5121e7be31c1c4ff12c47340da570e4f
Shtoner <zstoner57@gmail.com>
2022-12-10 12:51:28 -0600

    imgShouldWork

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
-import pro from '../public/images/pro.PNG'
+// import pro from '../public/images/pro.PNG'
 
 const name = 'Zachary Stoner'
 export const siteTitle = 'Next.js Sample Website'
@@ -31,7 +31,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src={pro}
+              src={"/images/pro.png"}
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -44,7 +44,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src={pro}
+                src={"/images/pro.png"}
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
