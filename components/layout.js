04a9610aad45222745a5dd14d4ce12fa599108c6
Shtoner <zstoner57@gmail.com>
2022-12-10 12:55:18 -0600

    trythis

--- components/layout.js
+++ components/layout.js
@@ -44,7 +44,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src={"/images/pro.png"}
+                src={"/images/profile.jpg"}
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
