8727f6fcc14d1985b59be19c5f221fd93a117567
Shtoner <zstoner57@gmail.com>
2022-12-10 12:57:11 -0600

    png

--- components/layout.js
+++ components/layout.js
@@ -31,7 +31,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src={"/images/pro.png"}
+              src={"/images/pro.PNG"}
               className={utilStyles.borderCircle}
               height={144}
               width={144}
