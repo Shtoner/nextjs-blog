4b57cbcb30f75d6b36e26a0cc2cfb61baf621cc9
Shtoner <zstoner57@gmail.com>
2022-12-10 12:27:03 -0600

    img

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import pro from '../public/images/pro.png'
 
 const name = 'Zachary Stoner'
 export const siteTitle = 'Next.js Sample Website'
@@ -30,7 +31,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/pro.png"
+              src={'/pro.png'}
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +44,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/pro.png"
+                src={"/pro.png"}
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
